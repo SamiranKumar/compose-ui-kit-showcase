66d4d1d480e320d33f0cf04cbfc3c5efd3897ea3
Samiran Kumar <samirankumar11@gmail.com>
2026-04-23 17:53:21 +0600

    "feat(utils): add Context extension for Toast messages"

---
+++ app/src/main/java/com/samirankumar11/uikitshowcase/utils/ToastUtils.kt
@@ -0,0 +1,9 @@
+package com.samirankumar11.uikitshowcase.utils
+
+import android.content.Context
+import android.widget.Toast
+
+
+fun Context.showToast(message: String, duration: Int = Toast.LENGTH_LONG) {
+    Toast.makeText(this, message, duration).show()
+}
