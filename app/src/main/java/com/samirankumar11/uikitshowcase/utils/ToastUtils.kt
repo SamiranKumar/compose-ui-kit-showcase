400a1919e55ffd865a2db32856db166df715f29a
Samiran Kumar <samirankumar11@gmail.com>
2026-04-23 18:08:38 +0600

    git commit -m "feat(utils): add showToast extension function"

--- app/src/main/java/com/samirankumar11/uikitshowcase/utils/ToastUtils.kt
+++ app/src/main/java/com/samirankumar11/uikitshowcase/utils/ToastUtils.kt
@@ -4,6 +4,12 @@ import android.content.Context
 import android.widget.Toast
 
 
+/**
+ * Shows a Toast message.
+ *
+ * @param message Text to display
+ * @param duration Toast duration (default: LENGTH_LONG)
+ */
 fun Context.showToast(message: String, duration: Int = Toast.LENGTH_LONG) {
     Toast.makeText(this, message, duration).show()
 }
