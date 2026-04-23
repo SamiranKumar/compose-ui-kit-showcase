a7de4221c80926bd227621e6be57cad7469e0214
Samiran Kumar <samirankumar11@gmail.com>
2026-04-23 20:31:35 +0600

    feat(utils): add showToast extension with KDoc documentation

--- app/src/main/java/com/samirankumar11/uikitshowcase/utils/ToastUtils.kt
+++ app/src/main/java/com/samirankumar11/uikitshowcase/utils/ToastUtils.kt
@@ -8,8 +8,8 @@ import android.widget.Toast
  * Shows a Toast message.
  *
  * @param message Text to display
- * @param duration Toast duration (default: LENGTH_LONG)
+ * @param duration Toast duration (default: LENGTH_LONG | LENGTH_SHORT)
  */
-fun Context.showToast(message: String, duration: Int = Toast.LENGTH_LONG) {
+fun Context.showToast(message: String, duration: Int = Toast.LENGTH_SHORT) {
     Toast.makeText(this, message, duration).show()
 }
