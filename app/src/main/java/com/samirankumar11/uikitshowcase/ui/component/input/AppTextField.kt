c5da08ab554774dc8aff561342ac69a13c811b70
Samiran Kumar <samirankumar11@gmail.com>
2026-04-24 22:51:09 +0600

    feat[TEXTFIELD]: apply design system styling

--- app/src/main/java/com/samirankumar11/uikitshowcase/ui/component/input/AppTextField.kt
+++ app/src/main/java/com/samirankumar11/uikitshowcase/ui/component/input/AppTextField.kt
@@ -1,9 +1,12 @@
 package com.samirankumar11.uikitshowcase.ui.component.input
 
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.samirankumar11.uikitshowcase.ui.theme.AppShapes
+import com.samirankumar11.uikitshowcase.ui.theme.AppSpacing
 import com.samirankumar11.uikitshowcase.ui.theme.ComposeUiKitShowcaseTheme
 
 @Composable
@@ -15,10 +18,11 @@ fun AppTextField(
     OutlinedTextField(
         value = value,
         onValueChange = onValueChange,
-        modifier = modifier,
+        modifier = modifier
+            .height(AppSpacing.textFieldHeight),
+        shape = AppShapes.medium,
     )
 }
-
 
 @Preview(showBackground = true)
 @Composable
