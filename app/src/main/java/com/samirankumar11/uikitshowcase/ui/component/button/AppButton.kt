f610ffbb6a247920d5d6e5469bd40d66ab13acfa
Samiran Kumar <samirankumar11@gmail.com>
2026-04-24 21:11:02 +0600

    feat[BUTTON]: apply design system styling

--- app/src/main/java/com/samirankumar11/uikitshowcase/ui/component/button/AppButton.kt
+++ app/src/main/java/com/samirankumar11/uikitshowcase/ui/component/button/AppButton.kt
@@ -1,10 +1,14 @@
 package com.samirankumar11.uikitshowcase.ui.component.button
 
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.samirankumar11.uikitshowcase.ui.theme.AppShapes
+import com.samirankumar11.uikitshowcase.ui.theme.AppSpacing
 import com.samirankumar11.uikitshowcase.ui.theme.ComposeUiKitShowcaseTheme
 
 @Composable
@@ -16,12 +20,19 @@ fun AppButton(
 ) {
     Button(
         onClick = onClick,
-        modifier = modifier,
+        modifier = modifier
+            .height(AppSpacing.buttonHeight),
         enabled = enabled,
+        shape = AppShapes.medium,
     ) {
-        Text(text = text)
+        Text(
+            text = text,
+            style = MaterialTheme.typography.labelLarge,
+        )
     }
 }
+
+
 @Preview(showBackground = true)
 @Composable
 private fun AppButtonPreview() {
