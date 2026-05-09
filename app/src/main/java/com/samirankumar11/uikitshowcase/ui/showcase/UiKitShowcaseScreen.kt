82f6c37af68c53832d299021d1cc74d35b9068c8
Samiran Kumar <samirankumar11@gmail.com>
2026-05-09 22:28:46 +0600

    feat[SHOWCASE][SCREEN]: update UiKitShowcaseScreen with components

--- app/src/main/java/com/samirankumar11/uikitshowcase/ui/showcase/UiKitShowcaseScreen.kt
+++ app/src/main/java/com/samirankumar11/uikitshowcase/ui/showcase/UiKitShowcaseScreen.kt
@@ -1,5 +1,4 @@
 package com.samirankumar11.uikitshowcase.ui.showcase
-
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,18 +13,29 @@ import com.samirankumar11.uikitshowcase.ui.showcase.screen.BottomBarShowcaseScre
 import com.samirankumar11.uikitshowcase.ui.showcase.screen.ButtonShowcaseScreen
 import com.samirankumar11.uikitshowcase.ui.showcase.screen.CardShowcaseScreen
 import com.samirankumar11.uikitshowcase.ui.showcase.screen.CheckboxShowcaseScreen
+import com.samirankumar11.uikitshowcase.ui.showcase.screen.CircularProgressShowcaseScreen
 import com.samirankumar11.uikitshowcase.ui.showcase.screen.DialogShowcaseScreen
 import com.samirankumar11.uikitshowcase.ui.showcase.screen.DividerShowcaseScreen
 import com.samirankumar11.uikitshowcase.ui.showcase.screen.EmptyStateShowcaseScreen
 import com.samirankumar11.uikitshowcase.ui.showcase.screen.IconButtonShowcaseScreen
 import com.samirankumar11.uikitshowcase.ui.showcase.screen.IconShowcaseScreen
+import com.samirankumar11.uikitshowcase.ui.showcase.screen.IconTextButtonShowcaseScreen
+import com.samirankumar11.uikitshowcase.ui.showcase.screen.LinearProgressShowcaseScreen
 import com.samirankumar11.uikitshowcase.ui.showcase.screen.ListItemShowcaseScreen
+import com.samirankumar11.uikitshowcase.ui.showcase.screen.OtpFieldShowcaseScreen
+import com.samirankumar11.uikitshowcase.ui.showcase.screen.OutlinedButtonShowcaseScreen
+import com.samirankumar11.uikitshowcase.ui.showcase.screen.PasswordFieldShowcaseScreen
 import com.samirankumar11.uikitshowcase.ui.showcase.screen.ProgressIndicatorShowcaseScreen
 import com.samirankumar11.uikitshowcase.ui.showcase.screen.RadioButtonShowcaseScreen
+import com.samirankumar11.uikitshowcase.ui.showcase.screen.RadioGroupShowcaseScreen
 import com.samirankumar11.uikitshowcase.ui.showcase.screen.SearchBarShowcaseScreen
+import com.samirankumar11.uikitshowcase.ui.showcase.screen.SegmentedButtonShowcaseScreen
 import com.samirankumar11.uikitshowcase.ui.showcase.screen.SnackBarShowcaseScreen
 import com.samirankumar11.uikitshowcase.ui.showcase.screen.SwitchShowcaseScreen
+import com.samirankumar11.uikitshowcase.ui.showcase.screen.TextButtonShowcaseScreen
 import com.samirankumar11.uikitshowcase.ui.showcase.screen.TextFieldShowcaseScreen
+import com.samirankumar11.uikitshowcase.ui.showcase.screen.TextShowcaseScreen
+import com.samirankumar11.uikitshowcase.ui.showcase.screen.ToggleButtonShowcaseScreen
 import com.samirankumar11.uikitshowcase.ui.showcase.screen.TopBarShowcaseScreen
 import com.samirankumar11.uikitshowcase.ui.theme.AppSpacing
 import com.samirankumar11.uikitshowcase.ui.theme.ComposeUiKitShowcaseTheme
@@ -78,6 +88,30 @@ fun UiKitShowcaseScreen(
         AvatarShowcaseScreen()
 
         EmptyStateShowcaseScreen()
+
+
+        OutlinedButtonShowcaseScreen()
+
+        TextButtonShowcaseScreen()
+
+        IconTextButtonShowcaseScreen()
+
+        PasswordFieldShowcaseScreen()
+
+        OtpFieldShowcaseScreen()
+
+        CircularProgressShowcaseScreen()
+
+        LinearProgressShowcaseScreen()
+
+        ToggleButtonShowcaseScreen()
+
+        SegmentedButtonShowcaseScreen()
+
+        RadioGroupShowcaseScreen()
+
+        TextShowcaseScreen()
+
     }
 }
 
