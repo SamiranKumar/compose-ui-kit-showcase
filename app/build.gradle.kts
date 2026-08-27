88c0921d2893795ff282f1093ff287b5505ae9d7
Samiran Kumar <samirankumar11@gmail.com>
2026-08-27 20:57:44 +0600

    feat[GRADLE][UPDATE]: update Gradle and dependency versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,6 @@ android {
 
 dependencies {
 
-    implementation(libs.androidx.compose.foundation.layout)
     // -----------------------------
     // Core Android
     // -----------------------------
@@ -53,12 +52,14 @@ dependencies {
     implementation(libs.androidx.activity.compose)
 
     // -----------------------------
-    // Compose (BOM manages versions)
+    // Compose BOM
     // -----------------------------
     implementation(platform(libs.androidx.compose.bom))
 
+    // Compose
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
 
